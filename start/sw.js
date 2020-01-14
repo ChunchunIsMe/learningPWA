23075c23028f950b53e289443a57b07f37e2bee5
linlichun <linlichun@meicai.cn>
2020-01-14 15:29:09 +0800

    pwa start

---
+++ start/sw.js
@@ -0,0 +1,42 @@
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.1.0/workbox-sw.js");
+const cacheStorageKey = "minimal-pwa-10";
+const cacheList = [
+  '/',
+  'index.html',
+  'main.css',
+  'test.png',
+  'run.js'
+]
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheStorageKey)
+      .then(cache => cache.addAll(cacheList))
+      .then(() => self.skipWaiting())
+  )
+})
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      if (res != null) {
+        return res;
+      }
+      return fetch(e.request.url)
+    })
+  )
+})
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames
+          .filter(cachename => cachename !== cacheStorageKey)
+          .map(cachename => caches.delete(cachename))
+      )
+    }).then(() => {
+      return self.clients.claim();
+    })
+  )
+})
